82fa29380aa3e81eb0f13f3a10cde5c9412e7327
c vw <dl1ycf@darc.de>
2021-09-10 09:51:04 +0200

    Removed un-used variable.

--- midi_menu.c
+++ midi_menu.c
@@ -49,7 +49,6 @@ enum {
   N_COLUMNS
 };
 
-static GtkWidget *parent_window=NULL;
 static GtkWidget *dialog=NULL;
 
 static GtkListStore *store;
@@ -773,10 +772,8 @@ void midi_menu(GtkWidget *parent) {
   GtkCellRenderer *renderer;
   GtkWidget *lbl;
 
-  parent_window=parent;
-
   dialog=gtk_dialog_new();
-  gtk_window_set_transient_for(GTK_WINDOW(dialog),GTK_WINDOW(parent_window));
+  gtk_window_set_transient_for(GTK_WINDOW(dialog),GTK_WINDOW(parent));
   gtk_window_set_title(GTK_WINDOW(dialog),"piHPSDR - MIDI");
   g_signal_connect (dialog, "delete_event", G_CALLBACK (delete_event), NULL);
 
